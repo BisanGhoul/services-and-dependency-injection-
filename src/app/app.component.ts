f5e5fe46f7e0cc2c14a647029151e2dc3e572089
BisanGhoul <bbisann@gmail.com>
2023-03-07 18:39:23 +0200

    create data service (account service)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountsService } from './accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AccountsService]
 })
-export class AppComponent {
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active'
-    },
-    {
-      name: 'Testaccount',
-      status: 'inactive'
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown'
-    }
-  ];
+export class AppComponent implements OnInit {
+  //we loop through the accounts in app.component.html file, so we still need them here
+  accounts: {name: string, status: string}[];
 
-  onAccountAdded(newAccount: {name: string, status: string}) {
-    this.accounts.push(newAccount);
+  constructor(private accountsService: AccountsService) {  }
+
+  ngOnInit(): void {
+      //note that sice account here is an array it is a reference type,
+      //so we are getting acces to the same exact array in accountsService 
+    this.accounts = this.accountsService.accounts;
   }
 
-  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
-    this.accounts[updateInfo.id].status = updateInfo.newStatus;
-  }
+  // accounts = [
+  //   {
+  //     name: 'Master Account',
+  //     status: 'active'
+  //   },
+  //   {
+  //     name: 'Testaccount',
+  //     status: 'inactive'
+  //   },
+  //   {
+  //     name: 'Hidden Account',
+  //     status: 'unknown'
+  //   }
+  // ];
+
+  // onAccountAdded(newAccount: {name: string, status: string}) {
+  //   this.accounts.push(newAccount);
+  // }
+
+  // onStatusChanged(updateInfo: {id: number, newStatus: string}) {
+  //   this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  // }
 }
